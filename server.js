bc2f2c0d2dbc55ea14fb4a2c1fc7ca88706d26a9
Austin Baird <austinbaird@austinbaird1s-MacBook-Pro-2.local>
2018-01-31 14:41:19 -0800

    testing heroku with login page public folder moved node stuff

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ app.use('/imgs',express.static(__dirname + '/imgs'));
 
 app.get('/',function(req,res){
         res.sendFile(__dirname+'/index.html');
-        });
+});
 
 server.listen(process.env.PORT || 8081,function(){
               console.log('Listening on '+server.address().port);
