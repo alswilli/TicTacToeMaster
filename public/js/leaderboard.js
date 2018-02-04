bd0d8c48f5258c369d7c5b67cd3ab7d3b02ebbcc
Derek <dtchiong@gmail.com>
2018-02-03 22:21:39 -0800

    -Integrated the switching leaderboard button to have it show players from each game. -Added template Previous & Next buttons to show more people in the leaderboard. -Will need to reverse the shown players, because Firebase only sorts by smallest. -Probably will need to add ranking score in the database, so we can sort by that instead of by  wins right now.

--- public/js/leaderboard.js
+++ public/js/leaderboard.js
@@ -1,12 +1,17 @@
 
+var maxRowsOnScreen;        //the max # of players that will be shown in the table at once
 var table;                  //the ID of the table element
-var activeBoard = "board1"; //the default game to show rankings for when loading leaderboard.html                 
+var activeBoard;            //the default game to show rankings for when loading leaderboard.html                 
 var snapshotArr;            //the snapshot as an array, of the leaderboard names
 
+
+//When the page is ready, intialize firebase and load the leaderboard for tictactoe
 $(document).ready(function() {
    console.log("START: $document.ready()");
 
+   maxRowsOnScreen = 10;
    table = document.getElementById("table"); //set table to be the table element
+   activeBoard = "TTT";
    
    // Initialize Firebase
    var config = {
@@ -19,162 +24,100 @@ $(document).ready(function() {
    };
    firebase.initializeApp(config);
    console.log("AFTER: firebase intialization");
-   createTable();
    
-   firebase.database().ref().child('leaderboard/TTT').orderByChild('Wins').on('value', function(snapshot) {
-      //console.log(snapshotToArray(snapshot));
-      snapshotArr = snapshotToArray(snapshot);
-      console.log(snapshotArr);
-      console.log("elem 0", snapshotArr[0]);
-      for (var i=0; i<snapshotArr.length; i++) {
-         console.log(snapshotArr[i].key + ' ' + snapshotArr[i].Wins + ' '+ snapshotArr[i].Losses);  
-      }
-      
-   });
-   
-   
+   getTopPlayersForGame(activeBoard);
 });
 
+/* This function is called when the button to show the board for a game is clicked
+ * Clears the current table and loads the players from the clicked game
+ */
+function switchToLeaderBoard(clickedBoard) {
+   $("#table tbody tr").remove(); 
+   getTopPlayersForGame(clickedBoard);
+   if (clickedBoard !== activeBoard) {
+      document.getElementById(clickedBoard).classList.add('w3-blue');
+      document.getElementById(activeBoard).classList.toggle('w3-blue');
+      activeBoard = clickedBoard;
+   }
+   
+}
+
+//Queries the DB for all the players from the given game and then creates the leaderboard table
+function getTopPlayersForGame(game) {
+      firebase.database().ref().child('leaderboard/'+game).orderByChild('Wins').on('value', function(snapshot) {
+      snapshotArr = snapshotToArray(snapshot);
+      //console.log(snapshotArr);
+      createTable();
+      
+   });
+}
+
+//Converts a snapshot into an array
 function snapshotToArray(snapshot) {
-   var returnArr = [];
+   var Arr = [];
    snapshot.forEach(function(childSnapshot) {
       var item = childSnapshot.val();
       item.key = childSnapshot.key;
       
-      returnArr.push(item);
+      Arr.push(item);
    });
-   return returnArr; 
+   return Arr; 
 };
 
-function switchToLeaderBoard(clickedBoard) {
-  //alert("lel");
-  if (clickedBoard !== activeBoard) {
-    document.getElementById(clickedBoard).classList.add('w3-blue');
-    document.getElementById(activeBoard).classList.toggle('w3-blue');
-    activeBoard = clickedBoard;
-  }
+
+/* Creates the table by adding a row for each player in the snapshotArr. If there are less than
+ * 10 rows, empty rows are added to pad the table to the constant size
+ */
+function createTable() {
+   var shownPlayers = 0;
+   var rank = 1;
+   console.log("START: createTable()");
+   for (var i=0; i<snapshotArr.length; i++) {
+      //console.log(snapshotArr[i].key + ' ' + snapshotArr[i].Wins + ' '+ snapshotArr[i].Losses);
+      addNewRow(rank, snapshotArr[i].key, snapshotArr[i].Wins, snapshotArr[i].Losses);
+      shownPlayers++;
+      rank++;
+   }
+   //Creates rows to fill in empty values 
+   for (var i=shownPlayers; i<maxRowsOnScreen; i++) {
+      addNewRow(rank, '','','');
+      rank++
+   }
+   console.log(snapshotArr[10]);
+   console.log(snapshotArr[10].key);   
 }
 
-function addNewRow(name, win, lose) {   
+//Adds a new row to the table
+function addNewRow(rank, name, win, lose) {   
    var row = document.createElement("TR");
    
    var td1 = document.createElement("TD");
    var td2 = document.createElement("TD");
    var td3 = document.createElement("TD");
    var td4 = document.createElement("TD");
+   var td5 = document.createElement("TD");
    
+   var winRate = ((win/lose * 100).toFixed(2)) + '%';
+   if ((win != 0) && (lose == 0)) { winRate = "100%"; }
+   if (name == '') { winRate = ''; }
+   
+   var rank_    = document.createTextNode(rank);
    var name_    = document.createTextNode(name);
    var win_     = document.createTextNode(win);
    var lose_    = document.createTextNode(lose);
-   var winRate_ = document.createTextNode(win/lose * 100 + '%');
+   var winRate_ = document.createTextNode(winRate);
    
-   td1.appendChild(name_);
-   td2.appendChild(win_);
-   td3.appendChild(lose_);
-   td4.appendChild(winRate_);
+   td1.appendChild(rank_);
+   td2.appendChild(name_);
+   td3.appendChild(win_);
+   td4.appendChild(lose_);
+   td5.appendChild(winRate_);
    
    row.appendChild(td1);
    row.appendChild(td2);
    row.appendChild(td3);
    row.appendChild(td4);
+   row.appendChild(td5);
    
-   table.appendChild(row);
-}
-
-function createTable() {
-    console.log("START: table()");
-    //var database = firebase.database();
-	/*
-    var table = document.createElement("TABLE");
-    table.setAttribute("id", "table1");
-    document.body.appendChild(table);
-    */
-
-    var row1 = document.createElement("TR");
-    var row2 = document.createElement("TR");
-    var row3 = document.createElement("TR");
-    var row4 = document.createElement("TR");
-      
-    var name1 = document.createTextNode("Jill Stein");
-    var win1  = document.createTextNode("1");
-    var lose1 = document.createTextNode("2");
-    var winrate1 = document.createTextNode("33.33%");
-    var td1a = document.createElement("TD");
-    var td1b = document.createElement("TD");
-    var td1c = document.createElement("TD");
-    var td1d = document.createElement("TD");
-  
-    var name2 = document.createTextNode("Dingus");
-    var win2  = document.createTextNode("2");
-    var lose2 = document.createTextNode("4");
-    var winrate2 = document.createTextNode("50.00%");
-    var td2a = document.createElement("TD");
-    var td2b = document.createElement("TD");
-    var td2c = document.createElement("TD");
-    var td2d = document.createElement("TD");
-  
-    var name3 = document.createTextNode("OpenGLuva");
-    var win3  = document.createTextNode("1");
-    var lose3 = document.createTextNode("7");
-    var winrate3 = document.createTextNode("0.14%");
-    var td3a = document.createElement("TD");
-    var td3b = document.createElement("TD");
-    var td3c = document.createElement("TD");
-    var td3d = document.createElement("TD");
-  
-    var name4 = document.createTextNode("Ugandan Warrior");
-    var win4  = document.createTextNode("10");
-    var lose4 = document.createTextNode("0");
-    var winrate4 = document.createTextNode("100.00%");
-    var td4a = document.createElement("TD");
-    var td4b = document.createElement("TD");
-    var td4c = document.createElement("TD");
-    var td4d = document.createElement("TD");
-    
-    document.getElementById("table").appendChild(row1);
-    document.getElementById("table").appendChild(row2);
-    document.getElementById("table").appendChild(row3);
-    document.getElementById("table").appendChild(row4);
-   
-    row1.appendChild(td1a);
-    row1.appendChild(td1b);
-    row1.appendChild(td1c);
-    row1.appendChild(td1d);
-  
-    row2.appendChild(td2a);
-    row2.appendChild(td2b);
-    row2.appendChild(td2c);
-    row2.appendChild(td2d); 
-  
-    row3.appendChild(td3a);
-    row3.appendChild(td3b);
-    row3.appendChild(td3c);
-    row3.appendChild(td3d);
-  
-    row4.appendChild(td4a);
-    row4.appendChild(td4b); 
-    row4.appendChild(td4c); 
-    row4.appendChild(td4d); 
-  
-    td1a.appendChild(name1);
-    td1b.appendChild(win1);
-    td1c.appendChild(lose1);
-    td1d.appendChild(winrate1);
-  
-    td2a.appendChild(name2);
-    td2b.appendChild(win2);
-    td2c.appendChild(lose2);
-    td2d.appendChild(winrate2);
-    
-    td3a.appendChild(name3);
-    td3b.appendChild(win3);
-    td3c.appendChild(lose3);
-    td3d.appendChild(winrate3);
-  
-    td4a.appendChild(name4); 
-    td4b.appendChild(win4);
-    td4c.appendChild(lose4);
-    td4d.appendChild(winrate4);
-   
-   addNewRow("fuck", 1, 3);
+   document.getElementById("table_body").appendChild(row);
 }
