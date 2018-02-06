30d91c2c1e0392c452177dd48a0ff93727cc3ccd
Derek <dtchiong@gmail.com>
2018-02-05 19:37:17 -0800

    Implemented buttons for showing next/prev players on the leacerboard

--- public/js/leaderboard.js
+++ public/js/leaderboard.js
@@ -3,25 +3,32 @@ var maxRowsOnScreen;        //the max # of players that will be shown in the tab
 var table;                  //the ID of the table element
 var activeBoard;            //the default game to show rankings for when loading leaderboard.html                 
 var snapshotArr;            //the snapshot as an array, of the leaderboard names
+var playerIndex;        //this is the initial index of the person to show on the leader, e.g - a value of 0 will be rank 1 player 
 
-
-//Firebase is already intialized, so load the leaderboard for tictactoe
+/* Firebase is already intialized, so load the leaderboard for tictactoe
+ * All these variables need to be initialized in .ready() because they need to be 
+ * used immediately, and initializing outside of .ready() doesn't work because
+ * jquery runs before javascript
+ */
 $(document).ready(function() {
    console.log("START: $document.ready()");
 
    maxRowsOnScreen = 10;
    table = document.getElementById("table"); //set table to be the table element
    activeBoard = "TTT";
+   playerIndex = 0;
 
    getTopPlayersForGame(activeBoard);
 });
 
-/* This function is called when the button to show the board for a game is clicked
- * Clears the current table and loads the players from the clicked game
+/* Toggles the color of the active shown game on the leaderboard when it is pressed.
+ * Clears the current table and loads the players from the clicked game.
  */
 function switchToLeaderBoard(clickedBoard) {
+   initPlayerIndex = 0;
    $("#table tbody tr").remove(); 
    getTopPlayersForGame(clickedBoard);
+   
    if (clickedBoard !== activeBoard) {
       document.getElementById(clickedBoard).classList.add('w3-blue');
       document.getElementById(activeBoard).classList.toggle('w3-blue');
@@ -30,7 +37,29 @@ function switchToLeaderBoard(clickedBoard) {
    
 }
 
-//Queries the DB for all the players from the given game and then creates the leaderboard table
+function getPrevPlayers() {
+   playerIndex -= 10;
+   
+   if (playerIndex < 0) { 
+      console.log("playerIndex < 0: Reverting to 0");
+      playerIndex = 0;
+   }else {
+      console.log("playerIndex:", playerIndex);
+      $("#table tbody tr").remove();
+      getTopPlayersForGame(activeBoard);
+   }
+}
+
+function getNextPlayers() {
+   playerIndex += 10;
+   
+   $("#table tbody tr").remove();
+   getTopPlayersForGame(activeBoard);
+}
+
+/* Queries the DB for all the players from the given game, converts the result to an array
+ * and then creates the leaderboard table
+ */
 function getTopPlayersForGame(game) {
       firebase.database().ref().child('leaderboard/'+game).orderByChild('Wins').on('value', function(snapshot) {
       snapshotArr = snapshotToArray(snapshot);
@@ -58,24 +87,27 @@ function snapshotToArray(snapshot) {
  */
 function createTable() {
    var shownPlayers = 0;
-   var rank = 1;
+   var rank = playerIndex + 1;
    console.log("START: createTable()");
-   for (var i=0; i<snapshotArr.length; i++) {
+   for (var i=playerIndex; i<snapshotArr.length; i++) {
       //console.log(snapshotArr[i].key + ' ' + snapshotArr[i].Wins + ' '+ snapshotArr[i].Losses);
+      if (snapshotArr[i] == undefined ) { continue; }
+      
       addNewRow(rank, snapshotArr[i].key, snapshotArr[i].Wins, snapshotArr[i].Losses);
       shownPlayers++;
       rank++;
    }
+   
    //Creates rows to fill in empty values 
    for (var i=shownPlayers; i<maxRowsOnScreen; i++) {
       addNewRow(rank, '','','');
       rank++
    }
-   //console.log(snapshotArr[10]);
-   //onsole.log(snapshotArr[10].key);   
 }
 
-//Adds a new row to the table
+/* Adds a new row to the table, given the person's name and stats
+ * If the name is '', then it's an empty row, 
+ */
 function addNewRow(rank, name, win, lose) {   
    var row = document.createElement("TR");
    
@@ -87,7 +119,7 @@ function addNewRow(rank, name, win, lose) {
    
    var winRate = ((win/lose * 100).toFixed(2)) + '%';
    if ((win != 0) && (lose == 0)) { winRate = "100%"; }
-   if (name == '') { winRate = ''; }
+   if (name == '')                { winRate = ''; }
    
    var rank_    = document.createTextNode(rank);
    var name_    = document.createTextNode(name);
