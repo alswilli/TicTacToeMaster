cdddf9649c587544afbe43c0f767f00f829ecb9f
Derek <dtchiong@gmail.com>
2018-03-10 06:01:33 -0800

    Modified reset leaderboard button to require user and password authentication

--- public/js/controllers/adminTools.js
+++ public/js/controllers/adminTools.js
@@ -12,30 +12,50 @@ angular.module('TicTacToeApp')
 
 
 /* Resets the stats for all players for each game.
- * First we grab the leaderboard reference, then we enter a loop for each game of the leaderboard.
- * Then we enter another loop for each player of the game, and in each of these iterations, we
- * reset the stats of the player.
+ * First we check if the current user logged in is authorized to use the button. Then we grab
+ * the leaderboard reference, then we enter a loop for each game of the leaderboard. Then we
+ * enter another loop for each player of the game, and in each of these iterations, we reset
+ * the stats of the player.
  */
 function resetAllLeaderboards() {
-   var confirmation = confirm("Reset leaderboards for each game?");
-   if (confirmation == false) {
+   if (userIsPrivileged(app.keyValue) != true) {
+      alert("Unauthorized User");
       return;
    }
 
-   //Fetches the leaderboard reference
-   firebase.database().ref().child('leaderboard').once('value').then(function (snapshot) {
+   var password = prompt("Reset leaderboards for each game? Enter password to confirm.");
+   if (password == null)
+      return;
 
-      //Loops for each game of the leaderboard
-      snapshot.forEach(function (childSnapshot) {
-         var gameType = childSnapshot.key;   //the game type
-         var gameData = childSnapshot.val(); //contains the player objects of the gametype
-
-         //Resets the stats for each user.
-         for (var playerID in gameData) {
-            resetUserScore(gameType, playerID, gameData[playerID]);
-         }
-      });
+   //Password authorization to proceed
+   firebase.database().ref('users/'+app.keyValue+'/password').once('value').then(function(snapshot) {
+      if (password == snapshot.val()) {
+         doResetAllLeaderboards();
+      }else {
+         alert("Incorrect Password");
+      }
    });
+
+   //Actually reset the board if we're authorized
+   function doResetAllLeaderboards() {
+      alert("All leaderboards reset");
+
+      //Fetches the leaderboard reference
+      firebase.database().ref().child('leaderboard').once('value').then(function (snapshot) {
+
+         //Loops for each game of the leaderboard
+         snapshot.forEach(function (childSnapshot) {
+            var gameType = childSnapshot.key;   //the game type
+            var gameData = childSnapshot.val(); //contains the player objects of the gametype
+
+            //Resets the stats for each user.
+            for (var playerID in gameData) {
+               resetUserScore(gameType, playerID, gameData[playerID]);
+            }
+         });
+      });
+
+   }
 }
 
 /* Resets a player's stats for a specific game using the gametype and userkey
@@ -49,4 +69,17 @@ function resetUserScore(gameType, playerID, playerData) {
       draw  : 0,
       rating: 1200
    });
+}
+
+/* Takes a userkey and checks if the user is priviledged or not
+ */
+function userIsPrivileged(userkey) {
+   var priviledgedUsers = [
+      "MLSKZ92Ea7WRjFE7HixlkPIz17N2"
+   ];
+
+   if (priviledgedUsers.indexOf(userkey) != -1) {
+      return true;
+   }
+   return false;
 }
